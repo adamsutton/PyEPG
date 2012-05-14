f0f5c68651853923fef7b1b58859532944e44b5e
Adam Sutton <dev@adamsutton.me.uk>
2012-05-14 11:52:36 +0100

    Start to work on packaging as a deb, some restructuring done and not yet working.

---
+++ dist/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Python setup script for pyepg
+
+# Imports
+import os, sys
+from distutils.core import setup
+
+# Directories
+dist_dir   = os.path.abspath(os.path.dirname(__file__))
+root_dir   = os.path.abspath(os.path.join(dist_dir, '..'))
+lib_dir    = os.path.join(root_dir, 'lib')
+script_dir = os.path.join(root_dir, 'scripts')
+
+# Auto build module list
+mods = []
+for p, ds, fs in os.walk(lib_dir):
+  if '__init__.py' in fs:
+    m = p.replace(lib_dir + '/', '').replace('/', '.')
+    mods.append(m)
+
+# Setup
+setup(
+  name             = 'pyepg',
+  version          = '0.0.1',
+  author           = 'Adam Sutton',
+  author_email     = 'dev@adamsutton.me.uk',
+  url              = '',
+  description      = '',
+  long_description = '',
+  packages         = mods,
+  package_dir      = { '' : 'lib' },
+  scripts          = [ 'scripts/pyepg.py' ],
+)
