bf03a60cb907d5fa70fdb86ef992714b2f27dd9a
Adam Sutton <dev@adamsutton.me.uk>
2012-10-24 12:59:11 +0100

    Add hires images to channel info.

--- lib/pyepg/package/uk_freetoairsathd.py
+++ lib/pyepg/package/uk_freetoairsathd.py
@@ -138,7 +138,10 @@ def _channels ():
       c.number = c.extra['sky_number'] = int(p[5])
       c.hd                             = p[6] == '1'
       c.radio                          = p[8] == '1'
-      c.image                          = p[9]
+      if (p[10])
+        c.image                        = p[10]
+      else
+        c.image                        = p[9]
 
       # Skip
       if not c.uri: continue
